df367a56baf5ad5f05c3515c3d27d5bbfb3c5d79
estela <estela.krein@universo.univates.br>
2020-08-10 20:10:08 -0300

    Projeto criado

---
+++ src/entidades/Idioma.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ *
+ * @author estel
+ */
+        
+@Entity
+@Table(name = "job")
+
+public class Idioma implements Serializable{
+    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "descricao")
+    private String descricao;
+
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
