2606161fd11dfdc0a9211363047a6bd9121905b5
DESKTOP-J2TMI5M\Acer <patriciaschneider2011@gmail.com>
2020-08-12 11:32:58 -0300

    novas configurações

---
+++ build/generated-sources/ap-source-output/entidades/Idioma_.java
@@ -0,0 +1,15 @@
+package entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Idioma.class)
+public abstract class Idioma_ {
+
+	public static volatile SingularAttribute<Idioma, Integer> codigo;
+	public static volatile SingularAttribute<Idioma, String> idioma;
+
+}
+
